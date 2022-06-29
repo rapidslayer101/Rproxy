524b0fd09b33d296e0acc60f6dd0c0157496f4b2
rapid <rapidslayer101@gmail.com>
2022-06-29 22:04:00 +0100

    GC1 - Heroku connection testing

--- proxy-client.py
+++ proxy-client.py
@@ -2,7 +2,7 @@
 
 import socket
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.connect(("172.16.233.130", 30678))
+conn.connect(("172.", 80))
 print("connected")
 conn.send("https://google.com".encode())
 data = conn.recv(1024)
